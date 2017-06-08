ff498c2b0cd18d4b83994b301848ab61d540e566
4xDMG <dajtait87@gmail.com>
2017-06-08 14:49:28 +1000

    Started work on PostsIndex component.

--- src/index.js
+++ src/index.js
@@ -4,30 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render() {
-    return <div>Hello!</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Goodbye!</div>
-  }
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-    <div>
-      <Route path="/hello" component={Hello} />
-      <Route path="/goodbye" component={Goodbye} />
-    </div>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
