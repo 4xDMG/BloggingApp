b9ce616ccfe668bed08878fa204988fff5ed442b
4xDMG <dajtait87@gmail.com>
2017-06-09 13:28:23 +1000

    Added form validation.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -38,8 +38,26 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
 
+    // Validate inputs from 'values'
+    if (!values.title || values.title.length < 3) {
+        errors.title = 'Enter a title that is at least 3 characters.';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories.';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content please.';
+    }
+
+    // If errors is empty, form will submit.
+    // If errors has any properties, redux-form assumes form is invalid.
+    return errors;
+}
 
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
