97b889b5254d1a5527380b233f44105d2be5d2d2
4xDMG <dajtait87@gmail.com>
2017-06-09 15:57:48 +1000

    Finished work on showing an individual post.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
+
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <Link to="/">Back To Index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   };
@@ -21,4 +32,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, fetchPost)(PostsShow);
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
