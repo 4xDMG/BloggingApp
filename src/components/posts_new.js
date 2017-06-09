02b8930e1cce960f5c6fb80b9728b4f3689306ba
4xDMG <dajtait87@gmail.com>
2017-06-09 15:17:24 +1000

    Worked on showing individual posts.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { createPost } from '../actions';
+import { connect } from 'react-redux';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -22,7 +24,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -74,4 +78,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,16 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+            return { ...state, [action.payload.data.id]: action.payload.data };
         default:
             return state;
     }
