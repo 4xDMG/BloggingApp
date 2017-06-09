beb99a9d047cc74f33c4b28322503ab53558daa8
4xDMG <dajtait87@gmail.com>
2017-06-09 11:07:23 +1000

    Added button to link new posts page.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
     componentDidMount() {
@@ -21,6 +22,11 @@ class PostsIndex extends Component {
     render() {
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
