753e8af9dc52d4559137edba093265b6f12feb6e
4xDMG <dajtait87@gmail.com>
2017-06-09 16:54:01 +1000

    Finished tutorial.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -5,39 +5,41 @@ import _ from 'lodash';
 import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
-    componentDidMount() {
-        this.props.fetchPosts();
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-    renderPosts() {
-        return _.map(this.props.posts, post => {
-            return (
-                <li className="list-group-item" key={post.id}>
-                    {post.title}
-                </li>
-            )
-        })
-    }
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            {post.title}         
+          </Link>
+        </li>
+      )
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="text-xs-right">
-                    <Link className="btn btn-primary" to="/posts/new">
-                        Add a Post
-                    </Link>
-                </div>
-                <h3>Posts</h3>
-                <ul className="list-group">
-                    {this.renderPosts()}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return { posts: state.posts };
+  return { posts: state.posts };
 }
 
 export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
+import { deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
+    if (!this.props.post) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
 
+  onDeleteClick() {
     const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -20,6 +29,12 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -32,4 +47,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
