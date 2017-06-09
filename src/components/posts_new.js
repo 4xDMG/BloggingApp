e0575ce0c78c4ce201780e41b9127a2dda5fc1aa
4xDMG <dajtait87@gmail.com>
2017-06-09 14:28:09 +1000

    Finished new post component exluding form submission to backend.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
@@ -41,6 +46,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
